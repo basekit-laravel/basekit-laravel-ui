aa6d611829b0fa498ebba2f4830d5009bc120e9f
Gergő Tar <dev@gergotar.com>
2026-08-17 10:24:42 +0200

    fix(navigation): add Escape key handler to dropdown menu

--- resources/views/components/navigation/dropdown-menu.blade.php
+++ resources/views/components/navigation/dropdown-menu.blade.php
@@ -12,7 +12,7 @@
     - default: Custom menu markup
 --}}
 
-<div class="bk-dropdown" x-data="{ open: false }" @click.away="open = false"
+<div class="bk-dropdown" x-data="{ open: false }" @click.away="open = false" @keydown.escape.window="open = false"
     @if ($openOn === 'hover') @mouseenter="open = true" @mouseleave="open = false" @endif {{ $attributes }}>
 
     {{-- Trigger --}}
